034c0631906e88740e9c72213fa28da4d7463847
Geert Guldentops <geertguldentops@jidoka.be>
2022-12-06 21:49:10 +0100

    Fourth test: Quality is never negative (requirement)

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,6 +2,8 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.groups.Tuple.tuple;
@@ -9,11 +11,29 @@ import static org.assertj.core.groups.Tuple.tuple;
 class GildedRoseTest {
 
 	@Nested
+	class Invariants {
+
+		@ParameterizedTest(name = "Item with name [{0}] should never have a negative quality")
+		@ValueSource(strings = { "Test item name", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros" })
+		void item_quality_is_never_negative(String itemName) {
+			var app = new GildedRose(new Item[] { new Item(itemName, 7, 0) });
+
+			app.updateQuality();
+
+			assertThat(app.items)
+					.hasSize(1)
+					.extracting(Item::getQuality)
+					.allSatisfy(quality -> assertThat(quality).isNotNegative());
+		}
+
+	}
+
+	@Nested
 	class AgedBrie {
 
 		@Test
 		void aged_brie_increases_in_quality() {
-			GildedRose app = new GildedRose(new Item[] { new Item("Aged Brie", 7, 5) });
+			var app = new GildedRose(new Item[] { new Item("Aged Brie", 7, 5) });
 
 			app.updateQuality();
 
@@ -32,7 +52,7 @@ class GildedRoseTest {
 
 		@Test
 		void sulfuras_does_not_change_quality() {
-			GildedRose app = new GildedRose(new Item[] { new Item("Sulfuras, Hand of Ragnaros", 7, 5) });
+			var app = new GildedRose(new Item[] { new Item("Sulfuras, Hand of Ragnaros", 7, 5) });
 
 			app.updateQuality();
 
@@ -47,7 +67,7 @@ class GildedRoseTest {
 
 		@Test
 		void sulfuras_is_never_sold() {
-			GildedRose app = new GildedRose(new Item[] { new Item("Sulfuras, Hand of Ragnaros", 25, 4) });
+			var app = new GildedRose(new Item[] { new Item("Sulfuras, Hand of Ragnaros", 25, 4) });
 
 			app.updateQuality();
 
