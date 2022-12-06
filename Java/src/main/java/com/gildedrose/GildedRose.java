e9690f160175d2532dba4a68820808277d159b64
Geert Guldentops <geertguldentops@jidoka.be>
2022-12-06 23:48:27 +0100

    Code clean-up: Introduce a new method that makes the "normal" item categorie an explicit concept, try encapsulating this method in item.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -19,8 +19,7 @@ class GildedRose {
     public void updateQuality() {
         for (Item item : items) {
             if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
-                if (!item.getName().equals("Aged Brie")
-                        && !item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.isNormal()) {
                     if (item.getQuality() > 0) {
                         item.setQuality(item.getQuality() - 1);
                     }
--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -17,6 +17,11 @@ public class Item {
         return name;
     }
 
+    public boolean isNormal() {
+        return !getName().equals("Aged Brie") &&
+                !getName().equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+
     public int getSellByDate() {
         return sellByDate;
     }
@@ -37,4 +42,5 @@ public class Item {
     public String toString() {
         return this.getName() + ", " + this.getSellByDate() + ", " + this.getQuality();
     }
+
 }
