9bd5d5aec41c16aabeecf21290d387a55b34ab27
Geert Guldentops <geertguldentops@jidoka.be>
2022-12-06 21:30:43 +0100

    First test: Aged brie increases in quality - add seams (getters) for testing

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,30 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.groups.Tuple.tuple;
 
 class GildedRoseTest {
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }
+	@Nested
+	class AgedBrie {
+
+		@Test
+		void aged_brie_increases_in_quality() {
+			GildedRose app = new GildedRose(new Item[] { new Item("Aged Brie", 7, 5) });
+
+			app.updateQuality();
+
+			assertThat(app.items)
+					.hasSize(1)
+					.extracting(Item::getName, Item::getSellIn, Item::getQuality)
+					.containsOnly(
+							tuple("Aged Brie", 6, 6)
+					);
+		}
+
+	}
 
 }
