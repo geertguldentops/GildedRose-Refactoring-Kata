910e5ccfc668f0cdf82f645f57021f6c41c8ea25
Geert Guldentops <geertguldentops@jidoka.be>
2022-12-06 22:42:22 +0100

    Code clean-up: Run formatter

--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -27,7 +27,7 @@ public class Item {
     }
 
     @Override
-   public String toString() {
+    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
 }
