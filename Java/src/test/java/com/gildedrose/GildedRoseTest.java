547108deea68bde47eccbdb6cd123083e4fdf6b1
Geert Guldentops <geertguldentops@jidoka.be>
2022-12-06 22:15:16 +0100

    Test: Extra boundary test when sell by date is now (i.e. 0) for Aged Brie

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -116,7 +116,7 @@ class GildedRoseTest {
 	class AgedBrie {
 
 		@Test
-		void aged_brie_increases_in_quality() {
+		void aged_brie_increases_in_quality_when_sell_by_date_positive() {
 			var app = new GildedRose(new Item[] { new Item(AGED_BRIE, 7, 5) });
 
 			app.updateQuality();
@@ -126,6 +126,21 @@ class GildedRoseTest {
 					.extracting(Item::getName, Item::getSellIn, Item::getQuality)
 					.containsOnly(
 							tuple(AGED_BRIE, 6, 6)
+					);
+		}
+
+		// TODO: Bug or feature?
+		@Test
+		void aged_brie_increases_in_quality_twice_as_fast_when_sell_by_date_now() {
+			var app = new GildedRose(new Item[] { new Item(AGED_BRIE, 0, 5) });
+
+			app.updateQuality();
+
+			assertThat(app.items)
+					.hasSize(1)
+					.extracting(Item::getName, Item::getSellIn, Item::getQuality)
+					.containsOnly(
+							tuple(AGED_BRIE, -1, 7)
 					);
 		}
 
