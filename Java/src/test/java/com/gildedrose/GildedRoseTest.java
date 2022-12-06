1bbef2d01715508f5cca2ab593c93edcfa8e6885
Geert Guldentops <geertguldentops@jidoka.be>
2022-12-06 22:17:57 +0100

    Test: When sell by date has passed, quality increases twice as fast for Aged Brie (missing test coverage missing for this case)

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -129,7 +129,7 @@ class GildedRoseTest {
 					);
 		}
 
-		// TODO: Bug or feature?
+		// TODO: Bug or feature? Not explicitly mentioned as a requirement
 		@Test
 		void aged_brie_increases_in_quality_twice_as_fast_when_sell_by_date_now() {
 			var app = new GildedRose(new Item[] { new Item(AGED_BRIE, 0, 5) });
@@ -141,6 +141,21 @@ class GildedRoseTest {
 					.extracting(Item::getName, Item::getSellIn, Item::getQuality)
 					.containsOnly(
 							tuple(AGED_BRIE, -1, 7)
+					);
+		}
+
+		// TODO: Bug or feature? Not explicitly mentioned as a requirement
+		@Test
+		void aged_brie_increases_in_quality_twice_as_fast_when_sell_by_date_has_passed() {
+			var app = new GildedRose(new Item[] { new Item(AGED_BRIE, -1, 5) });
+
+			app.updateQuality();
+
+			assertThat(app.items)
+					.hasSize(1)
+					.extracting(Item::getName, Item::getSellIn, Item::getQuality)
+					.containsOnly(
+							tuple(AGED_BRIE, -2, 7)
 					);
 		}
 
