2ebc5f864ecd7690c2e0211fc7fb6db2aa81e9fe
Geert Guldentops <geertguldentops@jidoka.be>
2022-12-07 08:29:57 +0100

    Code clean-up: Final clean-up of GildedRose itself, revert previous temporary refactoring (List can become Collection again), make getItems() package private since it is just a seam for the tests

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,28 +1,22 @@
 package com.gildedrose;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
 
 class GildedRose {
 
-    private final List<Item> items;
+    private final Collection<Item> items;
 
-    public GildedRose(List<Item> items) {
-        this.items = new ArrayList<>(items);
+    public GildedRose(Collection<Item> items) {
+        this.items = items;
     }
 
-    public Collection<Item> getItems() {
+    /* default */Collection<Item> getItems() {
         return Collections.unmodifiableCollection(items);
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.size(); i++) {
-            Item item = items.get(i);
-
-            item.updateQuality();
-        }
+        items.forEach(Item::updateQuality);
     }
 
 }
